4388a373b5db8624fddba57174bfd9dd0025daf9
rjmurphy3 <ryan.murphy@beautyco-lab.com>
2023-06-28 12:07:53 -0400

    Sample Fundamental Data Pull

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 # main.py
+import requests
 
 from config import API_KEY
 
 print(API_KEY)
+
+
+# replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
+url = 'https://www.alphavantage.co/query?function=OVERVIEW&symbol=IBM&apikey=' + API_KEY
+r = requests.get(url)
+data = r.json()
+
+print(data)
